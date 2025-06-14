e8c209749cbf4820821be7b113d414ae45d3e517
Valeriia Voloshyna <valeriavoloshina17@gmail.com>
2025-06-14 05:58:13 -0700

    Задача 1

--- goit-pycore-hw-03.py
+++ goit-pycore-hw-03.py
@@ -1,12 +1,14 @@
-import datetime
+from datetime import datetime
 def get_days_from_today(date):
     try:
-        #преобразуем строку в обьект даты
-        d = datetime.strptime(date, "%Y-%m-%d").date()
-
-        #вычисляем разницу между сегодняшним днем и заданной датой
-        return (datetime.today().date()- d).days
-    except:
+        #преобразуем строку в дату
+        input_date = datetime.strptime(date,"%Y-%m-%d").date()
+        #текущая дата 
+        today = datetime.today().date()
+        delta = today - input_date
+        return delta.days
+    except ValueError:
+        print ("Неверный формат")
         return None
-        
     
+print (get_days_from_today("2021-05-05"))
